389e7670e702d6bd7444e25dc801a56bf6b3e037
zhonglin3612 <842546605@qq.com>
2019-03-25 12:30:13 +0800

    sql文件

--- zhonglin/sql文件/h51812.sql
+++ zhonglin/sql文件/h51812.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50709
 File Encoding         : 65001
 
-Date: 2019-03-25 11:15:46
+Date: 2019-03-25 12:29:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -74,4 +74,50 @@ INSERT INTO `commodity` VALUES ('37', '中粮家佳康香嫩梅花肉 37', '7891
 INSERT INTO `commodity` VALUES ('38', '中粮家佳康香嫩梅花肉 38', '7891', '46', '4', '38', '654', '西式');
 INSERT INTO `commodity` VALUES ('39', '中粮家佳康香嫩梅花肉 39', '7891', '46', '6', '39', '456', '西式');
 INSERT INTO `commodity` VALUES ('40', '中粮家佳康香嫩梅花肉 40', '879', '6', '467', '40', '4566', '中式');
+
+-- ----------------------------
+-- Table structure for shop
+-- ----------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `id` int(11) DEFAULT NULL,
+  `标题` varchar(255) DEFAULT NULL,
+  `价格` int(255) DEFAULT NULL,
+  `评论` int(255) DEFAULT NULL,
+  `时间` int(255) DEFAULT NULL,
+  `重量` int(255) DEFAULT NULL,
+  `类别` varchar(255) DEFAULT NULL,
+  `用户` varchar(255) DEFAULT NULL,
+  `销量` int(11) DEFAULT NULL,
+  `数量` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of shop
+-- ----------------------------
+INSERT INTO `shop` VALUES ('5', '中粮家佳康香嫩梅花肉 5', '78978', '67', '5', '7', '中式', '5', '564', '1');
+INSERT INTO `shop` VALUES ('3', '中粮家佳康香嫩梅花肉 3', '456', '4567', '3', '67', '中式', '5', '4561', '6');
+INSERT INTO `shop` VALUES ('7', '中粮家佳康香嫩梅花肉 7', '45', '6', '7', '978', '中式', '5', '6451', '18');
+INSERT INTO `shop` VALUES ('2', '中粮家佳康香嫩梅花肉 2', '456', '7897', '2', '45', '中式', '5', '561', '1');
+
+-- ----------------------------
+-- Table structure for username
+-- ----------------------------
+DROP TABLE IF EXISTS `username`;
+CREATE TABLE `username` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userid` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of username
+-- ----------------------------
+INSERT INTO `username` VALUES ('1', '10086@qq.com', 'zlc438', 'zlcc');
+INSERT INTO `username` VALUES ('2', '19942195126', 'zlc438438', '郑亮琛');
+INSERT INTO `username` VALUES ('3', '842546605@qq.com', 'zhonglin3612', '钟林');
+INSERT INTO `username` VALUES ('4', '100826@qq.com', 'qweqwe', 'qweqwe');
+INSERT INTO `username` VALUES ('5', '管理员', 'qwe', 'qwe');
 SET FOREIGN_KEY_CHECKS=1;
